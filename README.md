92b775fb4cef763a803dd1f1aefdd8d58b142631
klmichael <87207684+klmichael@users.noreply.github.com>
2021-09-27 21:23:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project Title: PacMen Factory
+
+## Description of Project: 
+This project started with an assignment from Professor John Williams in my MIT XPro Bootcamp in week 7.  We were given starter files that included a PacMan factory to generate an array of PacMen and associated image elements on the html page.
+
+After ensuring that the PacMan would change direction upon hitting the bounds of the screen, I then added to coding to create the mouth chomping and flipping effects.
+
+I then added a third button, which will delete a random PacMan from both the array and the HTML page.
+
+*Original Author:* John Williams  
+*Additional Coding:* Kathryn Michael
+
+## How to Run:
+
+Download these six files and save them itogether in a directory:
+- index.html
+- pacmen.js
+- PacMan1.png
+- PacMan2.png
+- PacMan3.png
+- PacMan4.png
+
+Open index.html in your browser. You should now be able to play the game in your browser.
+
+## Roadmap of Future Improvements:
+
+In the future, I would like the user to be able to click on a specific PacMan to delete it from the game.  I would also like to update the styling so that the PacMen do not cover any of the buttons at any time.
+
+## License Information:
+
+**MIT License**
+
+*Copyright (c) 2020 John Williams*
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ pacmen.js
@@ -0,0 +1,83 @@
+// This array holds all the pacmen. Provided.
+const pacMen = []; 
+
+// I added these variables to support the chomping and flipping effects.
+var mouth = 0;
+const pacArray = [
+    ['PacMan1.png', 'PacMan2.png'],
+    ['PacMan3.png', 'PacMan4.png']
+];
+var direction = 0;
+    
+// Used for both velocity and position in makePac(). Provided.
+function setToRandom(scale) {
+    return {
+        x: Math.random() * scale,
+        y: Math.random() * scale
+    }
+}
+// Factory to make a PacMan at a random position with random velocity. Provided.
+// I edited newimg.src and the return to support the chomping and flipping.
+function makePac() {
+    // returns an object with random values scaled {x: 33, y: 21}
+    let velocity = setToRandom(20); // {x:?, y:?}
+    let position = setToRandom(200);
+    let illustration = {dir: 0, openClose: 0};
+    // Add image to div id = game
+    let game = document.getElementById('game');
+    let newimg = document.createElement('img');
+    newimg.style.position = 'absolute';
+    newimg.src = pacArray[direction][mouth];
+    newimg.width = 100;
+    // Set position here 
+    newimg.style.left = position.x + "px";
+    newimg.style.top = position.y + "px";
+    // Add new Child image to game
+    game.appendChild(newimg);
+    // Return details in an object
+    return {
+        position,
+        velocity,
+        illustration,
+        newimg
+    }
+}
+
+// Provided. I edited the x-axis position to support the chomping effect.
+function update() {
+    //loop over pacmen array and move each one and move image in DOM
+    pacMen.forEach((item) => {
+        checkCollisions(item);
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
+        item.illustration.openClose = (item.illustration.openClose + 1) % 2;
+        
+        item.newimg.style.left = item.position.x + "px";
+        item.newimg.style.top = item.position.y + "px";
+        item.newimg.src = pacArray[item.illustration.dir][item.illustration.openClose];
+    })
+    setTimeout(update, 100);
+}
+
+// When a collision on the x-axis is detected, I added the flipping effect.
+function checkCollisions(item) {
+    if (item.position.x + item.velocity.x + item.newimg.width >= window.innerWidth || item.position.x + item.velocity.x < 0) {
+        item.velocity.x = -1 * item.velocity.x;
+        item.illustration.dir = (item.illustration.dir + 1) % 2;
+    }
+    if (item.position.y + item.velocity.y + item.newimg.height >= window.innerHeight || item.position.y + item.velocity.y < 0) {
+        item.velocity.y = -1 * item.velocity.y;
+    }
+}
+
+// Provided.
+function makeOne() {
+    pacMen.push(makePac()); // add a new PacMan
+}
+
+function deleteOne() {
+    let i = Math.floor(Math.random() * pacMen.length);
+    var game = document.getElementById('game');
+    game.removeChild(pacMen[i].newimg);
+    pacMen.splice(i, 1);
+}
